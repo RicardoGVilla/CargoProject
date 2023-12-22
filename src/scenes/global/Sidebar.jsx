14a185a087286dccdffd0e6c8deab34bff29907d
Ricardo <ricardorgvillanueva@gmail.com>
2023-12-22 08:59:35 -0500

    setting up sidebar

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,5 +1,29 @@
+import {useState } from "react";
+import { ProSidebar, Menu, MenuItem} from "react-pro-sidebar";
+import 'react-pro-sidebar/dist/css/styles.css';
+import { Box, IconButton, Typography, useTheme} from '@mui/material';
+import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+import HomeOutlinedIcone from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodaytOutlined";
+
+
+
+
+
 const Sidebar = () => {
-  return <div>Sidebar</div>
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
+
+  return (
+    <Box>
+      
+    </Box>
+  )
 }
 
 export default Sidebar; 
